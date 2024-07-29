58c116f5de7fe15b950c60359745e3adea8e041d
Keshav <sharma.comkeshav@gmail.com>
2024-07-29 18:46:44 +0530

    Added text typing effect & some links

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,21 @@ window.addEventListener('scroll', function() {
     }
   });
   
+
+const txt = document.querySelector(".m1content .runtxt");
+const vals = ["inspirations.", "challenges.", "Learnings.", "Experience."];
+
+let valsIndex = 0;
+let chIndex = 0;
+changer();
+function changer() {
+  txt.innerHTML = `New ideas,<br>New ${vals[valsIndex].slice(0, chIndex)}<span class="curso">|</span>`;
+  chIndex++;
+  if (chIndex > vals[valsIndex].length) {
+    chIndex = 0;
+    valsIndex = (valsIndex + 1) % vals.length;
+    setTimeout(changer, 3000); // 1-second delay after a word is fully printed
+  } else {
+    setTimeout(changer, 100); // 200ms delay for each character
+  }
+}
